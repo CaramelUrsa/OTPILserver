4092e4e306b55eeeaebd4b22c4e34e397d3e8433
Logan Angellotti <loganangellotti@aliasappss-iMac.local>
2019-10-10 11:14:16 -0400

    not tested

---
+++ Server.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+app = express(),
+bodyParser = require('body-parser');
+port = process.env.PORT || 3000;
+
+const mysql = require('mysql');
+
+const mc = mysql.createConnection({
+    host        :   'localhost',
+    user        :   'root',
+    password    :   '',
+    database    :   'mydb'
+});
+
+mc.connect();
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+var routes = require('./app/routes/approuts');
+routes(app);
